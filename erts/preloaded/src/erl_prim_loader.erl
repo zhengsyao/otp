e7240ad7bd7a24f8776ec19864363f5d263e4f00
Shunichi Shinohara <shino.shun@gmail.com>
2012-04-19 09:43:05 +0900

    erts: work around prim archive read_file_info error (Co-Authored-by: Tuncer Ayaz)
    A file "abc.xyz" should not match to an archive named "abc".
    This patch requires equality or a separator character "/".

--- erts/preloaded/src/erl_prim_loader.erl
+++ erts/preloaded/src/erl_prim_loader.erl
@@ -1254,7 +1254,9 @@ do_name_split(ArchiveFile0, File) ->
 
 string_match([Char | File], [Char | Archive], RevTop) ->
     string_match(File, Archive, [Char | RevTop]);
-string_match(File, [], RevTop) ->
+string_match([] = File, [], RevTop) ->
+    {match, RevTop, File};
+string_match([$/ | _Rest] = File, [], RevTop) ->
     {match, RevTop, File};
 string_match(_File, _Archive, _RevTop) ->
     no_match.
